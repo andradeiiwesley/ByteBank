e6303fde7a08862f6b8ca1d7cedd833d0a5a7d51
Wesley Andrade <andradeiiwesley16@gmail.com>
2022-06-29 13:43:29 -0300

    first commit

---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,42 @@
+import { Router } from '@angular/router';
+import { TransferenciaService } from './../services/transferencia.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Transferencia } from 'models/transferencia.model';
+
+@Component({
+  selector: 'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls: ['./nova-transferencia.component.scss'],
+})
+export class NovaTransferenciaComponent {
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number;
+  destino: number;
+
+  constructor(private service: TransferenciaService, private router: Router) {}
+
+  transferir() {
+    console.log('Solicitada nova transferência');
+
+    const valorEmitir: Transferencia = {
+      valor: this.valor,
+      destino: this.destino,
+    };
+
+    this.service.adicionar(valorEmitir).subscribe(
+      (resultado) => {
+        console.log(resultado);
+        this.limparCampos();
+        this.router.navigateByUrl('extrato');
+      },
+      (error) => console.error(error)
+    );
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
